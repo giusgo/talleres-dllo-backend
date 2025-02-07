4c52cfbcf0531383cbc48d993e190833e58efab8
giusgo <giuseppeg@uninorte.edu.co>
2025-02-07 23:29:06 +0000

    taller01 done

---
+++ taller1.js
@@ -0,0 +1,54 @@
+function convertidorTemp(temp) {
+    return temp * 9 / 5 + 32;
+}
+
+function resolvedor(a, b, c, positive) {
+    let x = positive ? (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a) : (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+    return x;
+}
+
+function mejorParidad(number) {
+    return number % 2 === 0;
+}
+
+function bogo(arr) {
+    var shuffleCount = 0;
+    function shuffle(arr) {
+        var shuffled = [];
+        var rand;
+        while (arr.length !== 0) {
+            rand = Math.floor(Math.random() * arr.length);
+            shuffled.push(arr.splice(rand, 1)[0]);
+        }
+        return shuffled;
+    }
+
+    function sorted(shuffle) {
+        for (var i = 0; i < shuffle.length - 1; i++) {
+            if (shuffle[i] <= shuffle[i + 1]) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    do {
+        shuffleCount++;
+        arr = shuffle(arr);
+    } while (!sorted(arr));
+
+    return shuffleCount + ' | ' + arr.join();
+}
+
+function peorParidad(number) {
+    let even = number % 2 === 0;
+
+    // Bogo sort because yes
+    bogo([2, 1, 4, 3, 5, 6, 4, 2, 7, 8, 1]);
+
+    return even;
+}
+
+console.log(peorParidad(21));
